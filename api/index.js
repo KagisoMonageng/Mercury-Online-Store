570a4f84f0e4c75617ca175bf053cf3a9eaec01f
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2023-03-09 15:41:29 +0200

    Update index

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ var account = require('./app/routes/account')
 app.use(express.json());
 app.use(cors(corsOptions));
 
-app.listen(process.env.PORT|3000, () => {
+app.listen(3000, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 
